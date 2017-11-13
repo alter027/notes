eff9918f026e679a7f052c950cfa1bedde8fba5d
alter027 <wsjyun749@gmail.com>
2017-11-13 10:35:56 -0600

    Add files via upload

--- shell/insana.sh
+++ shell/insana.sh
@@ -3,10 +3,14 @@ curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 sudo apt install curl 
 curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh  
 bash Anaconda3-4.2.0-Linux-x86_64.sh   
-# can use conda therefore
-source ~/.bashrc   
 
-conda update spyder  
+echo yes | command
+printf 'y\nyes\nno\nmaybe\n' | ./script
+
+# can use conda therefore
+source ~/.bashrc
+
+conda update spyder
 pip install --upgrade pip
 
 pip install python3-xlib  
@@ -17,8 +21,8 @@ pip install lxml
 pip install selenium  
 pip install openpyxl  
 pip install PyPDF2  
-pip install imapclient  
+pip install imapclient
 pip install twilio  
-pip install pillow  
-pip install mss  
-pip install beautisoup4  
+pip install pillow
+pip install mss
+pip install beautisoup4
