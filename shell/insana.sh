2aa35a3b378ea8501147e2ddbcdf75b3fa0e98d6
alter027 <wsjyun749@gmail.com>
2017-11-14 00:08:36 +0800

    Create insana.sh

---
+++ shell/insana.sh
@@ -0,0 +1,24 @@
+cd /tmp   
+curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh  
+sudo apt install curl 
+curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh  
+bash Anaconda3-4.2.0-Linux-x86_64.sh   
+# can use conda therefore
+source ~/.bashrc   
+
+conda update spyder  
+pip install --upgrade pip
+
+pip install python3-xlib  
+pip install pyautogui
+pip install send2trash  
+# pip install requests  
+# pip install lxml  
+pip install selenium  
+# pip install openpyxl  
+pip install PyPDF2  
+pip install imapclient  
+pip install twilio  
+# pip install pillow  
+pip install mss  
+# pip install beautisoup4  
